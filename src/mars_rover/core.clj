58f7340aa2684a6e6f926f4807f0c04bfbedc43e
Joe Wright <joe@joejag.com>
2014-11-15 19:08:39 +0000

    Complete, all in one file

--- src/mars_rover/core.clj
+++ src/mars_rover/core.clj
@@ -1,4 +1,5 @@
-(ns mars-rover.core)
+(ns mars-rover.core
+  (:gen-class))
 
 (def compass-rotations
   {"N" {"L" "W", "R" "E"}
@@ -9,6 +10,59 @@
 (defn compass-rotate [current-direction way-to-rotate]
   (get-in compass-rotations [current-direction way-to-rotate]))
 
+(defn rotate [{:keys [cords direction]} way-to-rotate]
+  {:cords cords :direction (compass-rotate direction way-to-rotate)})
 
-(defn -main []
-  (println "hi"))
+
+
+(def cord-movers
+  {"N" (fn [[x y]] [x       (inc y)])
+   "E" (fn [[x y]] [(inc x) y])
+   "S" (fn [[x y]] [x       (dec y)])
+   "W" (fn [[x y]] [(dec x) y])})
+
+(defn move [{:keys [cords direction]}]
+  {:cords ((get cord-movers direction) cords) :direction direction})
+
+
+
+(def commands
+  {"M" (fn [robot _]         (move robot))
+   "L" (fn [robot direction] (rotate robot direction))
+   "R" (fn [robot direction] (rotate robot direction))})
+
+(defn command-parser [robot command]
+  ((get commands command (constantly robot)) robot command))
+
+
+
+(defn command-robot [starting-robot commands]
+  (reduce
+    (fn [updated-robot command]
+      (command-parser updated-robot command))
+    starting-robot
+    (clojure.string/split commands #"")))
+
+(defn- string-to-robot [str]
+  (let [splitted (clojure.string/split str #" ")
+        x (Integer. (first splitted))
+        y (Integer. (second splitted))
+        direction (last splitted)]
+    {:cords [x y] :direction direction}))
+
+(defn- robot-to-string [{:keys [cords direction]}]
+  (str (first cords) " " (second cords) " " direction))
+
+(defn- go [robot-str commands]
+  (robot-to-string
+    (command-robot (string-to-robot robot-str) commands)))
+
+(defn -main [& _]
+  (println "On separate lines please enter: grid, 1st robot, commands, 2nd robot, commands")
+  (let [_               (read-line)
+        first-robot     (read-line)
+        first-commands  (read-line)
+        second-robot    (read-line)
+        second-commands (read-line)]
+    (println (go first-robot first-commands))
+    (println (go second-robot second-commands))))
--- test/mars_rover/core_test.clj
+++ test/mars_rover/core_test.clj
@@ -6,14 +6,40 @@
        (tabular "can be rotated"
                 (fact (subject/compass-rotate ?current-direction ?way-to-rotate) => ?new-direction)
                 ?current-direction ?way-to-rotate ?new-direction
-                "N"                 "L"           "W"
-                "W"                 "L"           "S"
-                "S"                 "L"           "E"
-                "E"                 "L"           "N"
+                "N" "L" "W"
+                "W" "L" "S"
+                "S" "L" "E"
+                "E" "L" "N"
 
-                "N"                 "R"           "E"
-                "W"                 "R"           "N"
-                "S"                 "R"           "W"
-                "E"                 "R"           "S"))
+                "N" "R" "E"
+                "W" "R" "N"
+                "S" "R" "W"
+                "E" "R" "S"))
 
+(fact "can rotate a robot"
+      (fact (subject/rotate {:cords [1 1] :direction "N"} "R") => {:cords [1 1] :direction "E"}))
 
+(tabular "move x y cordinate based off compass direction"
+         (fact (subject/move {:cords [1 1] :direction ?direction}) => {:cords ?new-cords :direction ?direction})
+         ?direction ?new-cords
+         "N" [1 2]
+         "E" [2 1]
+         "S" [1 0]
+         "W" [0 1])
+
+(facts "rule parser"
+       (fact "understands move command"
+             (subject/command-parser ..robot.. "M") => irrelevant
+             (provided
+               (subject/move ..robot..) => anything))
+       (fact "understands rotate command"
+             (subject/command-parser ..robot.. "L") => irrelevant
+             (provided
+               (subject/rotate ..robot.. "L") => anything)))
+
+(facts "acceptance tests"
+       (tabular
+         (fact (subject/command-robot ?start-robot ?commands) => ?end-robot)
+         ?start-robot ?end-robot ?commands
+         {:cords [1 2] :direction "N"} {:cords [1 3] :direction "N"} "LMLMLMLMM"
+         {:cords [3 3] :direction "E"} {:cords [5 1] :direction "E"} "MMRMMRMRRM"))
